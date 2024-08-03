d9ec295cbb0b9ebb2eef3573ad9d76398dcf9490
bbhhainx <bbhhainx>
2024-08-03 13:15:49 +0700

    a

--- src/router.ts
+++ src/router.ts
@@ -2,9 +2,10 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 import HomeView from './components/HomeView.vue'
 import ResultEuro from './components/ResultEuro.vue'
 
+
 const routes = [
   { path: '/', component: HomeView },
-  { path: '/ket-Qua-Bong-Da-Euro', component: ResultEuro },
+  { path: '/ket', component: ResultEuro },
 ]
 
 export const router = createRouter({
