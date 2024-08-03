67e14f641fdf784046c53d154d893763defb0d2e
bbhhainx <bbhhainx>
2024-08-03 13:15:15 +0700

    a

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,7 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
 import { RouterView } from 'vue-router'
+import 'element-plus/dist/index.css'
+import 'vue3-virtual-scroller/dist/vue3-virtual-scroller.css'
 
 </script>
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,8 @@ import './style.css'
 import App from './App.vue'
 import { router } from './router'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import { RecycleScroller } from 'vue3-virtual-scroller'
-import 'vue3-virtual-scroller/dist/vue3-virtual-scroller.css'
+
 
 
 const APP = createApp(App)
