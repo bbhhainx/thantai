4327a22123b8e3962a62850609d86a4fcc8debbe
bbhhainx <bbhhainx>
2024-08-03 13:20:27 +0700

    a

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import App from './App.vue'
 import { router } from './router'
 import ElementPlus from 'element-plus'
 import { RecycleScroller } from 'vue3-virtual-scroller'
-
+import 'element-plus/dist/index.css'
+import 'vue3-virtual-scroller/dist/vue3-virtual-scroller.css'
 
 
 const APP = createApp(App)
--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,13 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from './components/HomeView.vue'
 import ResultEuro from './components/ResultEuro.vue'
 
-
 const routes = [
   { path: '/', component: HomeView },
-  { path: '/ket', component: ResultEuro },
+  { path: '/ket-Qua-Bong-Da-Euro', component: ResultEuro },
 ]
 
 export const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
